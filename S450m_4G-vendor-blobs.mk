084d5ad836307d8e60a2b8d0642ea45e83cd8ca0
rezvorck <rezvorck@gmail.com>
2016-12-31 13:44:10 +0500

    first commit

---
+++ S450m_4G-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/4Good/S450m_4G/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/4Good/S450m_4G/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/4Good/S450m_4G/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/4Good/S450m_4G/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/4Good/S450m_4G/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/4Good/S450m_4G/vendor,system/vendor)
